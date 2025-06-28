c4bfac50b3602cd7e112c365eebbae0c70142dc7
shinhee818 <153713451+shinhee818@users.noreply.github.com>
2025-06-28 22:54:49 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn AWS test
+          Learn AWS test2
         </a>
       </header>
     </div>

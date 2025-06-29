2fcf8e3641b347344c9e84ed0a11b83f0169c107
shinhee818 <153713451+shinhee818@users.noreply.github.com>
2025-06-29 21:54:43 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn cicd6
+          Learn cicd test
         </a>
       </header>
     </div>
